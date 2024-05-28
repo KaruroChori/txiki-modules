6cdd99f593ebdbef06bdcf509bc4e8588b323351
karurochari <nope>
2024-05-28 19:42:03 +0000

    fixed schema gen

--- scripts/gen-schemas.ts
+++ scripts/gen-schemas.ts
@@ -1,7 +1,10 @@
 #!/bin/env bun
 
+import { $ } from 'bun'
 import module from '../schemas/src/module.schema'
 import modules from '../schemas/src/modules.schema'
+
+await $`mkdir -p ./schemas/out`
 
 await Bun.write("./schemas/out/module.schema.json", JSON.stringify(module, null, 2))
 await Bun.write("./schemas/out/modules.schema.json", JSON.stringify(modules, null, 2))
